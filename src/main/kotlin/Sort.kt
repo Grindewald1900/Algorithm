f3b2725a6997d0e3b034b131d27c06bdae675d00
Grindewald1900 <grindewald1504@gmail.com>
2021-09-12 18:17:48 -0400

    [Update] Daily update

--- src/main/kotlin/Sort.kt
+++ src/main/kotlin/Sort.kt
@@ -5,9 +5,61 @@
  * **/
 
 fun main(){
-    println(bubbleSort(mutableListOf(8, 5, 2, 9, 5, 6, 3)))
-    println(insertionSort(mutableListOf(8, 5, 2, 9, 5, 6, 3)))
+//    println(bubbleSort(mutableListOf(8, 5, 2, 9, 5, 6, 3)))
+//    println(insertionSort(mutableListOf(8, 5, 2, 9, 5, 6, 3)))
+//    println(selectionSort(mutableListOf(8, 5, 2, 9, 5, 6, 3)))
+    println(threeNumberSort(mutableListOf(-2, -2, -2, -3, -3, -3, -3, -3, -3), mutableListOf(-2, -3, 0)))
 }
+/** Question-4 Solution 2 **/
+fun threeNumberSort(array: MutableList<Int>, order: List<Int>): List<Int> {
+    val orderCount = mutableListOf<Int>(0, 0, 0)
+    for (i in array.indices){
+        orderCount[order.indexOf(array[i])]++
+    }
+    var temp = 0
+    for (i in 0 .. 2){
+        for (j in temp until  temp + orderCount[i]){
+            array[j] = order[i]
+        }
+        temp += orderCount[i]
+    }
+    return array
+}
+
+/** Question-4 **/
+//fun threeNumberSort(array: MutableList<Int>, order: List<Int>): List<Int> {
+//    var index = 0
+//    if (array.size == 0) return listOf()
+//    for (i in order.indices){
+//        for (j in i+1 until array.size){
+//            if (array[j] == order[i]){
+//                array[j] = array[index]
+//                array[index] = order[i]
+//                index++
+//            }
+//        }
+//    }
+//    if(array.last() != order.last()){
+//        array.add(array.indexOf(array.last()), array.last())
+//        array.removeAt(array.size - 1)
+//    }
+//    return array
+//}
+
+/** Question-3 **/
+fun selectionSort(array: MutableList<Int>): List<Int> {
+    val result = mutableListOf<Int>()
+    while (array.isNotEmpty()){
+        var temp = Int.MAX_VALUE
+        array.forEach{
+            if (it < temp) temp = it
+        }
+        array.remove(temp)
+        result.add(temp)
+    }
+    return result
+}
+
 
 /** Question-2 **/
 fun insertionSort(array: MutableList<Int>): List<Int> {
