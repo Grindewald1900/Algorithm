b4e6b7967eb7a8695b18ba6ac7902855f1de94a4
Grindewald1900 <17788108425@163.com>
2022-04-04 19:04:47 -0400

    [Update] Daily update

--- src/main/kotlin/Searching.kt
+++ src/main/kotlin/Searching.kt
@@ -1,8 +1,62 @@
 import kotlin.math.floor
 
 fun main(){
-    binarySearch(listOf(1, 5, 23, 111), 35)
+    println(findThreeLargestNumbers(listOf(141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7)))
 }
+
+
+
+
+/** Question 2 - Find 3 largest number**/
+/**O(n) time and O(1) space **/
+fun findThreeLargestNumbers(array: List<Int>): List<Int> {
+    var largestNums = mutableListOf<Int>()
+    if(array.size < 3) return listOf()
+    if(array.size == 3) return sortNumbers(array)
+    largestNums = sortNumbers(array.subList(0, 3))
+    println(largestNums)
+    for (i in 3 until array.size){
+        if (array[i] > largestNums[0]){
+            println("I: $i Value: ${array[i]}")
+            largestNums[0] = array[i]
+            largestNums = sortNumbers(largestNums)
+            println(largestNums)
+        }
+    }
+    return largestNums
+}
+
+fun sortNumbers(list: List<Int>): MutableList<Int>{
+    val tempList = list.toMutableList()
+    val size = list.size - 1
+    for (i in 0 until size){
+        for (j in 0 until size - i){
+            if (tempList[j] > tempList[j+1]){
+                val temp = tempList[j]
+                tempList[j] = tempList[j+1]
+                tempList[j+1] = temp
+            }
+        }
+    }
+    return tempList
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /** Question 1 - Binary search**/
@@ -28,3 +82,5 @@ fun binarySearch(array: List<Int>, target: Int): Int {
     if (target == array[minIndex]) return minIndex
     return -1
 }
+
+
